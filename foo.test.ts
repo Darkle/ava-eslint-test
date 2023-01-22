e99f28af0d9d76a20b07a6d0258ba21136f86c04
Coop <coop.coding@gmail.com>
2023-01-22 12:53:21 +1100

    Update

--- foo.test.ts
+++ foo.test.ts
@@ -1,7 +1,7 @@
 import anyTest, { type TestFn } from 'ava'
 // import test from 'ava'
 
-const test = anyTest as TestFn<{ uniquePrefix: string }> & typeof anyTest
+const test = anyTest as TestFn<{ uniquePrefix: string }>
 
 // THIS SHOULD ERROR
 test.only('test 1', t => {
